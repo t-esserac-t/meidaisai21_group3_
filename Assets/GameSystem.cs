40a461ae9a5bfade7dbf03dc58a8ac4c0f632022
Hanae M / tesseract <issankatansochudoku@gmail.com>
2021-10-15 19:23:46 +0900

    Added title scene, thanks for Yoshi3110

---
+++ Assets/GameSystem.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameSystem : MonoBehaviour
+{
+   public void StartGame()
+    {
+        SceneManager.LoadScene ("test");
+    }
+
+    public void EndGame()
+    {
+#if UNITY_EDITOR
+    UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+    Application.OpenURL("http://sokon.jp/");
+#else
+        Application.Quit();
+#endif
+    }
+}
