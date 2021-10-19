9b1b6c71fbca17b96055cf8e7333370d28308e40
Hanae M / tesseract <issankatansochudoku@gmail.com>
2021-10-19 15:19:41 +0900

    敵に体力追加

--- Assets/EnemyTest.cs
+++ Assets/EnemyTest.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class EnemyTest : MonoBehaviour
 {
+    public int armorPoint;
+    public int armorPointMax = 1000;
+    int damage = 100;
     public GameObject explosion_prefab;
-
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        armorPoint = armorPointMax;
+
     }
 
     // Update is called once per frame
@@ -17,16 +21,24 @@ public class EnemyTest : MonoBehaviour
     {
         
     }
+
     void OnTriggerEnter(Collider other)
     {
         //接触したオブジェクトのタグが"Player"のとき
         if (other.tag == "Fire" )
         {
-            Instantiate(explosion_prefab, this.transform.position, Quaternion.identity);
+            //Instantiate(explosion_prefab, this.transform.position, Quaternion.identity);
             //thisとは: このスクリプトのこと。this.transform.positionは「このスクリプトがくっついているオブジェクトの位置」の意味
             //Debug.Log("attacked");
-            Destroy(this.gameObject);
-            
+            //  Destroy(this.gameObject);
+            armorPoint -= damage;
+            Debug.Log(armorPoint);
+
+            if (armorPoint <= 0)
+            {
+                Destroy(gameObject);
+                Instantiate(explosion_prefab, this.transform.position, Quaternion.identity);
+            }
         }
     }
 }
