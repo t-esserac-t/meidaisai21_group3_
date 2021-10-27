e095a463030ce30b3193a31804183e545b9f0f8a
Hanae M / tesseract <issankatansochudoku@gmail.com>
2021-10-27 15:37:21 +0900

    リザルトBGM追加、時間経過で当たり判定拡張

---
+++ Assets/ExpandCollisionDetection.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpandCollisionDetection : MonoBehaviour
+{
+    public CountDown countDown;
+    public Vector3 collisionDetection=new Vector3(1,1,1);
+    // Start is called before the first frame update
+    void Start()
+    {
+        collisionDetection.z = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(countDown.time<=50)
+        {
+            collisionDetection.z = 3; 
+        }
+        else
+        {
+            collisionDetection.z = 1;
+        }
+
+        this.transform.localScale=collisionDetection;
+      //  Debug.Log(countDown.time);
+    }
+}
